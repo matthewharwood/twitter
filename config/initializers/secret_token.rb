d6aa3b6a52e4821a52213b1c98b8a4c56642fc34
Matthew Harwood <matthhar12@gmail.com>
2013-08-29 14:24:37 -0700

    init push with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = 'ca0f4102a20293d5a8345c9a56327efdea7a81d54a397c07b251c87efd77a6434cfa414c4246fd7308c92e41e185d95f5420883de3cb93dbb1d12d3d8da72525'
